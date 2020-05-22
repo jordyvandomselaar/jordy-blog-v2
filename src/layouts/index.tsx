1acf02d5679c1e22b40352e15b6f39305994b732
Jordy <jordy.vandomselaar@dij.digital>
2020-05-22 21:47:44 +0200

    depl

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -10,7 +10,7 @@ import Image from "../components/Image";
 import Flex from "../components/Flex";
 import Tip from "../components/Tip";
 import Layout from "./Layout";
-import Head from "next/head";
+import Head from "next/head"; //
 
 const components = {
     pre: (props) => <div {...props} />,
